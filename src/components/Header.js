53ba3669e166c972834fda03826da329ddeec86d
Jimin Huh <jiminhuh95@gmail.com>
2021-02-01 21:42:22 -0600

    header html/css and working on showcase

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header box-shadow">
+            <nav className="nav">
+                <h1 id="title"><a href="/">Plantopedia</a></h1>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/plants">My Plants</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Showcase.js
@@ -0,0 +1,11 @@
+import background from '../assets/background.jpg'
+
+function Showcase() {
+    return (
+        <div className="showcase">
+            <img src={background} alt=""/>
+        </div>
+    )
+}
+
+export default Showcase
