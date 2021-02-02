9686325747b843b50288a7062659e022bf1493e7
Jimin Huh <jiminhuh95@gmail.com>
2021-02-02 09:45:45 -0600

    landing page completed

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+function Button() {
+    return (
+        <div>
+            <button className="startBtn">Click to Start</button>
+        </div>
+    )
+}
+
+export default Button
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,9 +1,19 @@
 import background from '../assets/background.jpg'
+import Button from './Button'
 
 function Showcase() {
     return (
         <div className="showcase">
             <img src={background} alt=""/>
+            <div className="showcase-text">
+                <div className="showcase-title">
+                    Welcome to Plantopedia
+                </div>
+                <div className="showcase-sub-title">
+                    Keep track of your plants and try to collect them all! 
+                </div>
+                <Button />
+            </div>
         </div>
     )
 }
