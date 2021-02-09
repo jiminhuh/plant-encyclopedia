c33df39b42f724888478b417e21e998c75b325f5
Jimin Huh <jiminhuh95@gmail.com>
2021-02-09 15:45:31 -0600

    created form for plant page

--- src/components/Plants.js
+++ src/components/Plants.js
@@ -8,16 +8,28 @@ function Plants() {
                 <form action="submit">
                     <label htmlFor="name">Name:</label>
                     <input type="text" name="name" id="name"/>
+                    <br/>
                     <label htmlFor="date">Date:</label>
                     <input type="date" name="date" id="date"/>
+                    <br/>
                     <label htmlFor="watering">Watering:</label>
+                    <br/>
+                    <label htmlFor="watering-monday">Monday</label>
                     <input type="checkbox" name="watering" id="watering-monday"/>
+                    <label htmlFor="watering-monday">Tuesday</label>
                     <input type="checkbox" name="watering" id="watering-tuesday"/>
+                    <label htmlFor="watering-monday">Wednesday</label>
                     <input type="checkbox" name="watering" id="watering-wednesday"/>
+                    <label htmlFor="watering-monday">Thursday</label>
                     <input type="checkbox" name="watering" id="watering-thursday"/>
+                    <label htmlFor="watering-monday">Friday</label>
                     <input type="checkbox" name="watering" id="watering-friday"/>
+                    <label htmlFor="watering-monday">Saturday</label>
                     <input type="checkbox" name="watering" id="watering-saturday"/>
+                    <label htmlFor="watering-monday">Sunday</label>
                     <input type="checkbox" name="watering" id="watering-sunday"/>
+                    <br/>
+                    <input type="button" value="submit"/>
                 </form>
             </div>
 
